4dde2b0791646b3420a05fa213233b1f700ac40a
vagrant <vagrant@homestead>
2019-03-25 09:51:37 +0000

    sukurtas modelis, sukurtas seederis, sukurta contacts migraciju lentele

---
+++ database/seeds/ProjectSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+      /* https://laravel.com/docs/5.8/eloquent */
+      /* Laravelyje "plain" SQL praktiškai nenaudojame,
+         naudojame Eloquent biblioteką */
+         
+      /* Kad galėtume dirbti su Eloquent reikia turėti modelį */
+
+
+    }
+}
