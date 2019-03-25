4cc554b322d6f30cbe98c25b523315670a7727f5
vagrant <vagrant@homestead>
2019-03-25 18:50:34 +0000

    seederis padarytas ir duomenu insertas per modeli

--- app/Project.php
+++ app/Project.php
@@ -1,6 +1,9 @@
 <?php
 
-namespace App;
+namespace App; // Galima isivaizduoti kaip folderį, kuriame yra Project klasė
+// šitos klasės kelias yra App\Project
+// Šitą klasę galima įsikelti su taisykle
+// use App\Project
 
 use Illuminate\Database\Eloquent\Model;
 
