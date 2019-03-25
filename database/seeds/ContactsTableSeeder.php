0f015052da1e99249838343cebdd73823ff771f6
vagrant <vagrant@homestead>
2019-03-25 19:17:36 +0000

    pridetas faker biblioteka

--- database/seeds/ContactsTableSeeder.php
+++ database/seeds/ContactsTableSeeder.php
@@ -12,12 +12,18 @@ class ContactsTableSeeder extends Seeder
      */
     public function run()
     {
-        $contact = new Contact;
-        // Stulpeliai ir reikšmės, kairėje pusėje stulpelių pavadinimai, o dešinėje jų reikšmės
-        $contact->full_name = 'Viktoras Lava';
-        $contact->email = 'info@viktoraslava.lt';
-        $contact->subject = 'Uzsakymas';
-        $contact->message = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-        $contact->save();
+        for ($i=0; $i < 20; $i++) {
+
+          $faker = Faker\Factory::create('lt_LT');
+
+          $contact = new Contact;
+          // Stulpeliai ir reikšmės, kairėje pusėje stulpelių pavadinimai, o dešinėje jų reikšmės
+          $contact->full_name = $faker->name;
+          $contact->email = $faker->email;
+          $contact->subject = 'Uzsakymas';
+          $contact->message = $faker->realText(600);
+          $contact->save();
+          
+        }
     }
 }
